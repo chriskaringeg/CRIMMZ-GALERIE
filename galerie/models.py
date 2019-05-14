69ea1bc518633c714c57e4f2ea91aa20fa4f9cb4
chriskaringeg <ckaringe@gmail.com>
2019-05-14 11:42:03 +0300

    deploy

--- galerie/models.py
+++ galerie/models.py
@@ -55,7 +55,7 @@ class Image(models.Model):
 
     @classmethod
     def search_by_category(cls,search_term):
-        image = cls.objects.filter(category__category__icontains=search_term).all()
+        image = cls.objects.filter(category__name__icontains=search_term).all()
         return image
 
     '''Method to filter database results'''
